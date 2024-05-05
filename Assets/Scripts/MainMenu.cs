7b2a9d96cd31c4c538c9514617cbc0d079424378
youngmin-dev <166973661+youngmin-dev@users.noreply.github.com>
2024-05-05 21:25:20 +0900

    d
    d

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickStartGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
